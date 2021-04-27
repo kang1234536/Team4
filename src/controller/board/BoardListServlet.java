fe4fa5619ae3190f84efac9335f8af6e69f730ce
qwodud <qwodud@kakao.com>
2021-04-27 14:41:25 +0900

    boardListServlet Update

--- src/controller/board/BoardListServlet.java
+++ src/controller/board/BoardListServlet.java
@@ -28,7 +28,8 @@ public class BoardListServlet extends HttpServlet {
 		int totalData = (int) Math.ceil(dao.getCount()/10.0);
 		request.setAttribute("totalData", totalData);
 		request.setAttribute("board_list", blist);
-		request.setAttribute("list_length", blist.size()+1);
+		if(next==0) request.setAttribute("list_length", dao.getCount()+1);
+		else request.setAttribute("list_length", (dao.getCount()+1)-10*next);
 		RequestDispatcher rd = request.getRequestDispatcher("boardList.jsp");
 		rd.forward(request, response);
 	}
